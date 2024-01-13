b21b785920f7000758dd7288ebe6aacc794e532a
hassaan-ahmad-digital <hassaanahmaddigital@gmail.com>
2024-01-13 14:45:44 +0500

    create header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { About, Home, Profile, SignIn, SignOut, SignUp } from "./pages"
+import { Header } from "./components"
 
-// type Props = {}
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-out" element={<SignOut/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
