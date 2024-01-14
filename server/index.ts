297efb28bdf683f3edf5d69c85efb521ace490a9
hassaan-ahmad-digital <hassaanahmaddigital@gmail.com>
2024-01-14 10:15:21 +0500

    mongodb connected and user model created

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,17 @@
-import express from 'express'
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to Database"))
+  .catch(err => console.error(err))
 
 const app = express()
 
-const port = 3001
+const port = 3000
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
