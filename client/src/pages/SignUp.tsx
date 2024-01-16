d7c07d9fa946f2b7fb11bd05123deaf237622271
hassaan-ahmad-digital <hassaanahmaddigital@gmail.com>
2024-01-16 14:59:46 +0500

    complete sign up page ui

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // type Props = {}
 
 export const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" name="username" id="username" placeholder='username' className='border p-3 rounded-lg' />
+        <input type="email" name="email" id="email" placeholder='email' className='border p-3 rounded-lg' />
+        <input type="password" name="password" id="password" placeholder='password' className='border p-3 rounded-lg' />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign up</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p className="">Have an account?</p>
+        <Link to="/sign-in">
+          <span className='text-blue-700'>Sign In</span>
+        </Link>
+      </div>
+    </div>
   )
 }
